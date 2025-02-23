75dd20220f1a68208b9e6797909dd9d715da3623
alvaldes86 <76670909+alvaldes86@users.noreply.github.com>
2025-02-23 13:58:24 -0500

    Add project files.

---
+++ ArrayAlgorithims.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cook_Prep
+{
+    public class ArrayAlgorithims
+    {
+
+        public int SequentialSearch(string[] inputarr, string value)
+        {
+            bool found = false;
+
+            int i = 0;
+
+            int position = -1;
+
+            while (!found && i < inputarr.Length)
+            {
+                if (inputarr[i] == value)
+                {
+                    found = true;
+                    position = i; break;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return position;
+        }
+
+        //overloaded for integer
+        public int SequentialSearch(int[] inputArr, int value)
+        {
+            bool found = false;
+
+            int i = 0;
+
+            int position = -1;
+
+            while (!found && i < inputArr.Length)
+            {
+                if (inputArr[i] == value)
+                {
+                    found = true;
+                    position = i; break;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return position;
+        }
+
+        public int[] CopyArray(int[] inputarr)
+        {
+            int[] arrayCopy = new int[inputarr.Length];
+
+            for (int i = 0; i <= inputarr.Length -1; i++)
+            {
+                arrayCopy[i] = inputarr[i];
+            }
+
+            return arrayCopy;
+        }
+
+
+        public bool CompareArrays(int[] array1, int[] array2)
+        {
+            if(array1.Length != array2.Length)
+            {
+                return false;
+            }
+
+
+            bool equal = true;
+            int i = 0;
+            while(equal && i < array1.Length)
+            {
+                if(array1[i] != array2[i])
+                {
+                    equal = false;
+                }
+                i++;
+            }
+            return equal;
+        }
+
+        public int Sum(int[] array)
+        {
+            int total = 0;
+
+            foreach (int i in array)
+            {
+                total += i;
+            }
+
+            return total;
+        }
+
+        public double Average(int[] array)
+        {
+            double average = 0;
+
+            foreach (int i in array)
+            {
+                average += i;
+            }
+
+            average = average / array.Length;
+
+            return average;
+        }
+
+        public int FindHighest(int[] array)
+        {
+            int highest = array[0];
+
+            foreach (int i in array)
+            {
+                if (i > highest)
+                {
+                    highest = i;
+                }
+            }
+
+            return highest;
+        }
+
+        public int FindLowest(int[] array)
+        {
+            int lowest = array[0]; 
+
+            foreach (int i in array)
+            {
+                if (i < lowest)
+                {
+                    lowest = i;
+                }
+            }
+
+            return lowest;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using Cook_Prep;
+using System.Runtime.CompilerServices;
+
+
+//Console.WriteLine("Enter a word to check if it a palindrome");
+
+
+
+//string input = Console.ReadLine();
+
+//string output = string.Empty;
+
+//for (int i = input.Length -1; i >= 0; i--)
+//{
+//    output += input[i];
+
+//}
+
+//if(output.ToLower() == input.ToLower())
+//{
+//    Console.WriteLine("the word is a palidrome");
+//    Console.WriteLine($"You entered {input}");
+//    Console.WriteLine($"Reversed is {output}");
+//}
+//else
+//{
+//    Console.WriteLine("Try another word");
+//}
+
+ArrayAlgorithims utility = new ArrayAlgorithims();
+
+
+
+//string[] names = { "alain", "kire", "yoe", "alain", "pete", "kire" };
+
+//Console.WriteLine(utility.SequentialSearch(names, "yoe"));
+
+//Console.WriteLine($"the name was found on position {utility.SequentialSearch(names, "yoe")}");
+
+
+int[] numbers = { 77, 2, 3, 4, 2, 15, 3 };
+
+//Console.WriteLine($"the name was found on position {arrAlg.SequentialSearch(numbers, 2)}");
+
+
+int[] newCopy = utility.CopyArray(numbers);
+int[] arrayDifferetn = { 1, 2, 5, 1, 9 };
+
+//Console.WriteLine(arrAlg.CompareArrays(newCopy, arrayDifferetn) ? "The arrays are equal" : "Arrays are different");
+
+//Console.WriteLine(utility.Sum(arrayDifferetn));
+
+//Console.WriteLine(utility.Average(numbers));
+
+
+Console.WriteLine( numbers.Sum());
+Console.WriteLine(numbers.Average());
+
+Console.WriteLine(utility.FindHighest(numbers));
+Console.WriteLine(utility.FindLowest(numbers));
